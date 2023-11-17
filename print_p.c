42b341c4a6d99082c727a4306a73376784b4e9ec
root <Eithanbtabarez14@gmail.com>
2023-11-17 07:31:13 -0800

    implement Print a Percent

--- print_p.c
+++ print_p.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
+ * print_percent - Print character '%'.
+ * @P: percent to print.
  *
- *
+ * Return: Always 1 if is success.
  */
-int print_percent(va_list P) 
+
+int print_percent(va_list P)
 {
-	(void)P; 
+	(void)P;
 	_putchar('%');
-       	return (1);
+	return (1);
 }
